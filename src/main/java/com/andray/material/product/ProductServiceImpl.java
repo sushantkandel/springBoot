7830330c0036790a58c960a2b62549bb1842aad1
sushantkandel1995 <peaceandray@gmail.com>
2019-05-16 09:51:36 +0545

    Service Interface implementation completed

---
+++ src/main/java/com/andray/material/product/ProductServiceImpl.java
@@ -0,0 +1,45 @@
+package com.andray.material.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+   @Autowired
+    ProductRepository productRepository;
+    public List<Product> getAllProduct(Long categoryId) {
+        List<Product> products = new ArrayList<>();
+        productRepository.findByCategoryId(categoryId).forEach(products::add);
+        return products;
+    }
+
+    public Product getProductById(Long id) throws Exception {
+        Optional<Product> topic = productRepository.findById(id);
+        if (topic == null) {
+            throw new Exception("Jpt exception");
+        }
+        return topic.get();
+    }
+
+    public void addProduct(Product product) {
+        productRepository.save(product);
+    }
+
+    public void updateProduct(Product product) {
+
+        productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) throws Exception {
+
+        getProductById(id);
+        productRepository.deleteById(id);
+
+    }
+
+}
+
