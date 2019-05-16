9e12fa81fd741b88819700baeb747e0e22b9500b
sushantkandel1995 <peaceandray@gmail.com>
2019-05-16 09:45:01 +0545

    Service interface is defined

--- src/main/java/com/andray/material/category/CategoryService.java
+++ src/main/java/com/andray/material/category/CategoryService.java
@@ -1,46 +1,12 @@
 package com.andray.material.category;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-@Service
-public class CategoryService {
-    @Autowired
-    CategoryRepository categoryRepository;
-
-
-    public List<Category> getAllCategory() {
-        List<Category> categories = new ArrayList<>();
-        categoryRepository.findAll().forEach(categories::add);
-        return categories;
-    }
-
-
-    public Category getCategoryById(Long id) throws Exception {
-        Optional<Category> category = categoryRepository.findById(id);
-        if (category == null) {
-            throw new Exception("Jpt exception");
-        }
-        return category.get();
-    }
-
-    public void addCategory(Category category) {
-        categoryRepository.save(category);
-    }
-
-    public void updateCategory(Category category) {
-
-        categoryRepository.save(category);
-    }
-
-    public void deleteCategory(Long id) throws Exception {
-
-        getCategoryById(id);
-        categoryRepository.deleteById(id);
-
-    }
+public interface CategoryService {
+    public List<Category> getAllCategory();
+    public Category getCategoryById(Long id) throws Exception;
+    public void addCategory(Category category);
+    public void updateCategory(Category category);
+    public void deleteCategory(Long id) throws Exception;
 
 }
